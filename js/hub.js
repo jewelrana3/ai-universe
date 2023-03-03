e393931e9bb4acf3c27dbabefd3f2aa1fbb53d1b
jewelrana3 <juyelrana7752@gmail.com>
2023-03-03 14:41:48 +0600

    commit menu

--- js/hub.js
+++ js/hub.js
@@ -5,20 +5,28 @@ const loadData = async () => {
     loadData2(data.data.tools);
 }
 const loadData2 = phones => {
-    // console.log(phones)
+    console.log(phones[0])
     const appContainer = document.getElementById('app-container');
     phones.forEach(element => {
         const card = document.createElement('div');
         card.classList.add('col');
         card.innerHTML = `
-        <div class="card">
-        <img src="" class="card-img-top" alt="...">
+        <div class="card h-100">
+        <img src=${element.image} class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">${element.name}</h5>
+          <p class="p-0 m-0">1.${element.features[0]}</p>
+          <p class="p-0 m-0">2.${element.features[1]}</p>
+          <p class="p-0 m-0">3.${element.features[2]}</p>
+
+          <hr>
+          <div class="">
+                <h5>${element.name}</h5>
+                <p class="p-0 m-0">${element.published_in}</p>
+                
+          </div>
         </div>
-    </div>
+      </div>
         `;
         appContainer.appendChild(card)
     });
