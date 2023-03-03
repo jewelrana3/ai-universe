1bd79415e169a968dd23b0d16d1ce9b23d249cc6
jewelrana3 <juyelrana7752@gmail.com>
2023-03-03 12:34:02 +0600

    manoey

--- js/hub.js
+++ js/hub.js
@@ -1,22 +1,27 @@
-const loadData = () =>{
+const loadData = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => loadData2(data.data))
+    const res = await fetch(url);
+    const data = await res.json();
+    loadData2(data.data.tools);
 }
-const loadData2 = user =>{
-    console.log(user)
-    
+const loadData2 = phones => {
+    // console.log(phones)
+    const appContainer = document.getElementById('app-container');
+    phones.forEach(element => {
+        const card = document.createElement('div');
+        card.classList.add('col');
+        card.innerHTML = `
+        <div class="card">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.</p>
+        </div>
+    </div>
+        `;
+        appContainer.appendChild(card)
+    });
 }
-
-
-
-
-
-
-
-
-
-
 
 loadData();
