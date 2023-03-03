5d5acb241cd60e4eff2997fd9f36eacc5dc024e5
jewelrana3 <juyelrana7752@gmail.com>
2023-03-03 15:19:10 +0600

    batch

--- js/hub.js
+++ js/hub.js
@@ -5,7 +5,7 @@ const loadData = async () => {
     loadData2(data.data.tools);
 }
 const loadData2 = phones => {
-    console.log(phones[0])
+    console.log(phones)
     const appContainer = document.getElementById('app-container');
     phones.forEach(element => {
         const card = document.createElement('div');
@@ -18,18 +18,29 @@ const loadData2 = phones => {
           <p class="p-0 m-0">1.${element.features[0]}</p>
           <p class="p-0 m-0">2.${element.features[1]}</p>
           <p class="p-0 m-0">3.${element.features[2]}</p>
+          
 
           <hr>
-          <div class="">
-                <h5>${element.name}</h5>
-                <p class="p-0 m-0">${element.published_in}</p>
-                
+          <div class="card-footer d-flex justify-content-between">
+                <div>
+                    <h5>${element.name}</h5>
+                    <p class="p-0 m-0">${element.published_in}</p>
+                </div>
+                 <div class="d-flex justify-content-center align-items-center">
+                     <i class=" fas fa-arrow-right"  onclick="showDetails('${element.id}')"></i>
+                 </div
           </div>
         </div>
       </div>
         `;
         appContainer.appendChild(card)
     });
+};
+const showDetails = _id =>{
+    const url =  `https://openapi.programming-hero.com/api/ai/tool/${_id}`
+    console.log(url)
 }
+// onclick="showDetails('${}')"
+ 
 
 loadData();
