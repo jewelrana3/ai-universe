b7e3bc04413d2e85dd5522f2e0b3cdcc5395b4d5
jewelrana3 <juyelrana7752@gmail.com>
2023-03-04 11:31:17 +0600

    fetch add

--- js/hub.js
+++ js/hub.js
@@ -3,11 +3,11 @@
 //     const res = await fetch(url);
 //     const data = await res.json();
 //     loadData2(data.data.tools);}
-const loadData = () =>{
+const loadData = () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     fetch(url)
-    .then(res => res.json())
-    .then(data => loadData2(data.data.tools))
+        .then(res => res.json())
+        .then(data => loadData2(data.data.tools))
 }
 
 const loadData2 = phones => {
@@ -42,34 +42,38 @@ const loadData2 = phones => {
         appContainer.appendChild(card)
     });
 };
-const showDetails = news =>{
-    const url =  `https://openapi.programming-hero.com/api/ai/tool/${news}`
+const showDetails = news => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${news}`
     console.log(url)
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayShowDetails(data.data))
+        .then(res => res.json())
+        .then(data => displayShowDetails(data.data))
 }
 
-const displayShowDetails = (deatails) =>{
+const displayShowDetails = (deatails) => {
     console.log(deatails)
-    const {image_link,name,features,published_in} = deatails;
-    document.getElementById('modal-body').innerHTML=`
+    const { image_link, input_output_examples, features, published_in } = deatails;
+    document.getElementById('modal-body').innerHTML = `
     <div class="col" >
-        <div class="card h-100">
-            <img src=${image_link[0]} class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="p-0 m-0">1.${features[0]}</p>
-            <p class="p-0 m-0">2.${features[1]}</p>
-            <p class="p-0 m-0">3.${features[2]}</p>
-            <div class="card-footer d-flex justify-content-between">
-                <div>
-                    <h5>${name}</h5>
-                    <p class="p-0 m-0">${published_in}</p>
+        <div class="card h-100 d-flex">
+            <div>
+                <img src=${image_link[0]} class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${input_output_examples[0].input}</h5>
+                <p class="card-title p-0 m-0">${input_output_examples[0].output}</p>
+            </div>
+
+            <div>
+                <p class="p-0 m-0">1.${features[0]}</p>
+                <p class="p-0 m-0">2.${features[1]}</p>
+                <p class="p-0 m-0">3.${features[2]}</p>
+                <div class="card-footer d-flex justify-content-between">
+                    <div>
+                        <h5>${name}</h5>
+                        <p class="p-0 m-0">${published_in}</p>
+                    </div>
                 </div>
             </div>
-            </div>
-            
         </div>
     </div>
         
@@ -79,7 +83,7 @@ const displayShowDetails = (deatails) =>{
 // document.getElementById('submit').addEventListener('click',function(){
 //     const submitText = document.getElementById('submit')
 // })
- 
+
 
 loadData();
 
